62f8cc351b02b7df973c4ee25a7bc15d8d8c1af7
Yasser Itani <itani_yasser92@yahoo.com>
2013-09-19 22:29:35 -0400

    Game

---
+++ Images.java
@@ -0,0 +1,57 @@
+ackage PKM;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Images extends JFrame{
+	public static void main (String[] args){
+		
+		DisplayMode dm = new DisplayMode(800,600,16, DisplayMode.REFRESH_RATE_UNKNOWN);
+		Images m = new Images();
+		m.run(dm);
+	}
+	
+	private Screen s;
+	private Image bg;
+	private Image pic;
+	private boolean loaded;
+	
+	//run method
+	public void run(DisplayMode dm){
+		setBackground(Color.LIGHT_GRAY);
+		setForeground(Color.RED);
+		setFont(new Font("Arial", Font.PLAIN, 24));
+		loaded = false;
+		
+		s = new Screen();
+		try{
+			s.setFullScreen(dm, this);
+			loadpics();
+			try{
+				Thread.sleep(5000);
+			}catch(Exception ex){}
+		}finally{
+			s.restoreScreen();
+		}
+	}
+	
+	//loads pictures
+	private void loadpics() {
+		bg = new ImageIcon("C:\\Users\\Yasser Is Awesome!!!\\Pictures\\pkmfield1.jpg").getImage();
+		pic = new ImageIcon("C:\\Users\\Yasser Is Awesome!!!\\Pictures\\star.jpg").getImage();
+		loaded = true;
+		repaint();
+	}
+
+	public void paint(Graphics g){
+		if(g instanceof Graphics2D){
+			Graphics2D g2 = (Graphics2D)g;
+			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		}
+		if(loaded){
+			g.drawImage(bg,0,0,null);
+			g.drawImage(pic, 170,180, null);
+		}
+	}
+}
